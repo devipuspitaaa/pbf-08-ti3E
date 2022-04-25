36525aaf0db51512f9e4a1cdde7c635168633fb7
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-04-25 10:12:13 +0700

    praktikum firebase

---
+++ src/10_Firebase/js-10/src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwPJcmr1oaCfxJX1Nqgn8llwqIEMFajAE",
+    authDomain: "js10-firebase.firebaseapp.com",
+    projectId: "js10-firebase",
+    storageBucket: "js10-firebase.appspot.com",
+    messagingSenderId: "468642001831",
+    appId: "1:468642001831:web:88beb0579f777452947c82",
+    measurementId: "G-FJ4NC3Z3W9"
+  };
+  
